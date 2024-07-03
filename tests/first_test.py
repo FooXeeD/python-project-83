c756aeaf87314d66375f80d2b20eec5d14d1b1b9
fooxeed <fooxeed@mail.ru>
2024-07-03 22:55:02 +1000

    Update 0.4.7

---
+++ tests/first_test.py
@@ -0,0 +1,8 @@
+def test_request_example(client):
+    """
+    Test if the client is running
+    """
+    response = client.get("/")
+    string = "Анализатор страниц"
+    bytes_data = string.encode('utf-8')
+    assert bytes_data in response.data
