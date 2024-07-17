3db02fb215c5f44e94f01703d2876f5220113345
fooxeed <fooxeed@mail.ru>
2024-07-18 02:21:01 +1000

    Fix Flake8 5

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -6,7 +6,7 @@ from psycopg2.extras import NamedTupleCursor
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = postgresql://fooxeed:4QNoKsp1i1ifqaJa3rLYLwZKUBM4T25K@dpg-cq0kpsiju9rs73auptc0-a/db_twcf
+DATABASE_URL = 'postgresql://fooxeed:4QNoKsp1i1ifqaJa3rLYLwZKUBM4T25K@dpg-cq0kpsiju9rs73auptc0-a/db_twcf'
 
 engine = create_engine(DATABASE_URL, echo=True)
 
