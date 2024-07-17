1653511fe1c2d7406702eeb0458237eb7ab82233
fooxeed <fooxeed@mail.ru>
2024-07-18 02:12:49 +1000

    Fix Flake8 3

--- page_analyzer/models/basemodel.py
+++ page_analyzer/models/basemodel.py
@@ -4,9 +4,6 @@ from sqlalchemy import TIMESTAMP
 from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-engine = create_engine(DATABASE_URL)
 
 Base = declarative_base()
 
